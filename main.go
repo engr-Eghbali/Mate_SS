1e77594f82ae363f88e44b00a792ac17061ba263
reza <reza@>
2019-03-11 22:08:58 +0000

    hello world !

--- main.go
+++ main.go
@@ -4,26 +4,29 @@ import (
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/gin-gonic/gin"
+    "fmt"
+	//"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
-func main() {
+func determineListenAddress() (string, error) {
 	port := os.Getenv("PORT")
-
 	if port == "" {
-		log.Fatal("$PORT must be set")
+	  return "", fmt.Errorf("$PORT not set")
 	}
-
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
-
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
-
-	router.Run(":" + port)
-}
+	return ":" + port, nil
+  }
+  func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello World")
+  }
+  func main() {
+	addr, err := determineListenAddress()
+	if err != nil {
+	  log.Fatal(err)
+	}
+	http.HandleFunc("/", hello)
+	log.Printf("Listening on %s...\n", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+	  panic(err)
+	}
+  }
