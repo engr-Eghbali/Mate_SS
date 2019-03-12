7956d75a43ac150c942c4d0f23eef356825d5872
reza <reza@>
2019-03-12 13:45:22 +0000

    mail test

--- main.go
+++ main.go
@@ -1,46 +1,85 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"net/smtp"
 	"os"
-    "fmt"
+
 	//"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
+//////////////port detemine
 func determineListenAddress() (string, error) {
 	port := os.Getenv("PORT")
 	if port == "" {
-	  return "", fmt.Errorf("$PORT not set")
+		return "", fmt.Errorf("$PORT not set")
 	}
 	return ":" + port, nil
-  }
+}
 
+///////////////////////////////////////////////////////////////////
 
-  func hello(w http.ResponseWriter, r *http.Request) {
+////////////send mail
+func send(body string) (bool er) {
+	from := "trane2sfc@gmail.com"
+	pass := "Wakeuptrane2sfc$"
+	to := "engr.eghbali@gmail.com"
+
+	msg := "From: " + from + "\n" +
+		"To: " + to + "\n" +
+		"Subject: Hello there\n\n" +
+		body
+
+	err := smtp.SendMail("smtp.gmail.com:587",
+		smtp.PlainAuth("", from, pass, "smtp.gmail.com"),
+		from, []string{to}, []byte(msg))
+
+	if err != nil {
+		log.Printf("smtp error: %s", err)
+		return false
+	}
+
+	log.Print("sent, visit http://foobarbazz.mailinator.com")
+	return true
+}
+
+////////////////////////////////////////////////////////
+
+///////////ajax response and parse
+func hello(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/javascript")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
-	if r.Method == "POST"{
+	if r.Method == "POST" {
 		r.ParseForm()
 
-		id:=r.Form["id"][0]
-        fmt.Fprintln(w, "Here it is: "+id)
-	}else{
+		id := r.Form["id"][0]
+		mail := send(id)
+		if mail {
+
+			fmt.Fprintln(w, "yeeeaaaaah !")
+
+		} else {
+			fmt.Fprintln(w, "not sent")
+		}
+
+	} else {
 		fmt.Fprintln(w, "not post")
 	}
-	
-  }
 
+}
 
+////////////////////////////////////////////////////////////////////////////
 
-  func main() {
+func main() {
 
 	addr, err := determineListenAddress()
 	if err != nil {
-	  log.Fatal(err)
+		log.Fatal(err)
 	}
 
 	http.HandleFunc("/", hello)
@@ -48,7 +87,7 @@ func determineListenAddress() (string, error) {
 	log.Printf("Listening on %s...\n", addr)
 
 	if err := http.ListenAndServe(addr, nil); err != nil {
-	  panic(err)
+		panic(err)
 	}
 
-  }
+}
